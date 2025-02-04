f5d317ad822360c1e3636b0b620b4e61d8a47385
Jose Lima <jalimag0206@gmail.com>
2025-02-04 17:30:09 -0300

    feat(backend): add in-memory student repository for tests

---
+++ backend/src/repository/in-memory/in-memory-student-repository.ts
@@ -0,0 +1,38 @@
+import { Prisma, Student } from "@prisma/client";
+import { IStudentRepository } from "../student-repository";
+
+export class InMemoryStudentRepository implements IStudentRepository {
+  public students: Student[] = [];
+
+  async getAll() {
+    return this.students;
+  }
+
+  async create(data: Prisma.StudentCreateInput) {
+    const student = {
+      ra: "123456",
+      name: data.name,
+      email: data.email,
+      cpf: data.cpf,
+    };
+    this.students.push(student);
+    return student;
+  }
+
+  async findByEmail(email: string) {
+    const student = this.students.find((item) => item.email === email);
+    if (!student) {
+      return null;
+    }
+    return Promise.resolve(student);
+  }
+
+  async findByRa(ra: string) {
+    const student = this.students.find((item) => item.ra === ra);
+
+    if (!student) {
+      return null;
+    }
+    return student;
+  }
+}
