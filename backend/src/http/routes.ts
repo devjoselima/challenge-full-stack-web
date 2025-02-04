4f0809f80be2ae133589811b9df625dbef847678
Jose Lima <jalimag0206@gmail.com>
2025-02-04 15:35:46 -0300

    feat(backend): add GetAllStudentsController

--- backend/src/http/routes.ts
+++ backend/src/http/routes.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from "fastify";
-import { createStudentController } from "./controllers/";
+import {
+  createStudentController,
+  GetAllStudentsController,
+} from "./controllers/";
 
 export async function appRoutes(app: FastifyInstance) {
+  app.get("/students", GetAllStudentsController);
   app.post("/students", createStudentController);
 }
