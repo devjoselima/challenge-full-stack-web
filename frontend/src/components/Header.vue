8478910cff2d5e20a702999be8fe19e9bb4ceee0
Jose Lima <jalimag0206@gmail.com>
2025-02-05 15:05:45 -0300

    feat(frontend): add Header component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <SideBar v-model="isDrawerOpen" />
+
+  <v-app-bar flat class="border-b">
+    <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"></v-app-bar-nav-icon>
+    <v-app-bar-title>Consulta de alunos</v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from "vue";
+  import SideBar from '@/components/SideBar.vue'
+  const isDrawerOpen = ref(true)
+</script>
