c5cba7d32f7a2665f26c6582b620de9e9b8f74f7
Jose Lima <jalimag0206@gmail.com>
2025-02-05 20:05:45 -0300

    feat(frontend): add StudentToolbar component

---
+++ frontend/src/components/StudentToolbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container class="d-flex align-center justify-space-between ga-4">
+    <v-text-field
+      v-model="search"
+      label="Buscar por RA"
+      variant="outlined"
+      append-inner-icon="mdi-magnify"
+      clearable
+      @click:append-inner="onSearch"
+      @keyup.enter="onSearch"
+      style="max-width: 400px;"
+    />
+    <v-btn color="primary">Cadastrar aluno</v-btn>
+  </v-container>
+
+</template>
+ 
+<script setup>
+import { ref } from 'vue';
+
+const search = ref('');
+
+const onSearch = () => {
+  console.log(search.value);
+};
+</script>
