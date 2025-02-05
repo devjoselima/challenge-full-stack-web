62fa3061365da1789d9b4a48287cc1a5822720b0
Jose Lima <jalimag0206@gmail.com>
2025-02-05 15:05:27 -0300

    feat(frontend): add SideBar component

---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-navigation-drawer flat>
+    <v-list>
+      <v-img
+      class="mb-4"
+      height="150"
+      src="@/assets/grupo-a-logo.png"
+    />
+    <v-list-item prepend-icon="mdi-account">Alunos</v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts" setup>
+  
+</script>
