b415fe745da69f10cac3605d53112fb6356fa433
Jose Lima <jalimag0206@gmail.com>
2025-02-06 00:43:34 -0300

    refactor(frontend): separtes table headers to utils file

---
+++ frontend/src/utils/student-table-headers.ts
@@ -0,0 +1,6 @@
+export const studentTableHeaders = [
+  { title: "Registro Acadêmico", key: "ra" },
+  { title: "Nome", key: "name" },
+  { title: "CPF", key: "cpf" },
+  { title: "Ações", key: "actions", sortable: false },
+]
