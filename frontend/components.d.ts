ff6cc4a408da4431e341a075812bbff817d8066a
Jose Lima <jalimag0206@gmail.com>
2025-02-06 16:17:22 -0300

    feat(frontend): add StudentForm component and new student route

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -7,13 +7,13 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    DeleteStudentModa: typeof import('./src/components/delete-student-moda.vue')['default']
     DeleteStudentModal: typeof import('./src/components/delete-student-modal.vue')['default']
     Header: typeof import('./src/components/header.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SideBar: typeof import('./src/components/side-bar.vue')['default']
+    StudentForm: typeof import('./src/components/student-form.vue')['default']
     StudentTable: typeof import('./src/components/student-table.vue')['default']
     StudentToolbar: typeof import('./src/components/student-toolbar.vue')['default']
     TableActions: typeof import('./src/components/table-actions.vue')['default']
