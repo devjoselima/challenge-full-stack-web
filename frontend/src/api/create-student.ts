d851d1cfef75e89e4faee08869e94e4e366460d7
Jose Lima <jalimag0206@gmail.com>
2025-02-06 16:14:26 -0300

    feat(frontend): add createStudent API function for student creation

---
+++ frontend/src/api/create-student.ts
@@ -0,0 +1,16 @@
+import type { Student } from "@/@types/student";
+import { api } from "./axios"
+
+interface CreateStudentBody {
+  ra: string;
+  name: string;
+  email: string;
+  cpf: string;
+}
+
+export const createStudent = async ({ ra, name, email, cpf }: CreateStudentBody) => {
+  
+  const response = await api.post('/students', { ra, name, email, cpf });
+
+  return response.data
+}
