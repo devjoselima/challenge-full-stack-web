dc30a262da98d9c76c879ff8ec5b4507c2dcae1b
Jose Lima <jalimag0206@gmail.com>
2025-02-06 00:42:31 -0300

    chore(frontend): rename file name

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts" setup>
-  import Header from '@/components/Header.vue';
+  import Header from '@/components/header.vue';
   import StudentToolbar from '@/components/student-toolbar.vue';
   import StudentTable from '@/components/student-table.vue';
 </script>
