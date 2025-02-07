d659e611fdabf6c05123ec586b31c49c23e901a4
Jose Lima <jalimag0206@gmail.com>
2025-02-07 16:44:25 -0300

    feat(frontend): add logout button on header

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -4,11 +4,28 @@
   <v-app-bar flat class="border-b">
     <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"></v-app-bar-nav-icon>
     <v-app-bar-title>Consulta de alunos</v-app-bar-title>
+    <v-tooltip text="Sair" location="bottom">
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props" @click="handleLogout">
+          <v-icon>mdi-logout</v-icon>
+        </v-btn>
+      </template>
+    </v-tooltip>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from "vue";
-  import SideBar from '@/components/side-bar.vue'
-  const isDrawerOpen = ref(true)
+import { ref } from "vue";
+import { useAuthStore } from "@/store/auth";
+import { useRouter } from "vue-router";
+import SideBar from '@/components/side-bar.vue'
+
+const isDrawerOpen = ref(true);
+const authStore = useAuthStore();
+const router = useRouter();
+
+const handleLogout = () => {
+  authStore.logout();
+  router.push("/login");
+};
 </script>
