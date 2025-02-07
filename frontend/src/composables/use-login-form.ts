b6fe0736756283631cc7dad9ea96e8cf0db5e664
Jose Lima <jalimag0206@gmail.com>
2025-02-07 19:24:54 -0300

    feat(frontend): update minimum password length of 6 characters in login form

--- frontend/src/composables/use-login-form.ts
+++ frontend/src/composables/use-login-form.ts
@@ -3,6 +3,7 @@ import { toTypedSchema } from '@vee-validate/zod'
 import { z } from 'zod'
 import { useAuthStore } from '@/store/auth'
 import { useRouter } from "vue-router";
+import "vue3-toastify/dist/index.css";
 
 export const useLoginForm = () => {
   const authStore = useAuthStore()
@@ -11,7 +12,7 @@ export const useLoginForm = () => {
   const loginSchema = toTypedSchema(
     z.object({
       email: z.string({ required_error: 'O e-mail é obrigatório' }).email('E-mail inválido'),
-      password: z.string({ required_error: 'A senha é obrigatória' }),
+      password: z.string({ required_error: 'A senha é obrigatória' }).min(6, { message: 'A senha deve ter no mínimo 6 caracteres' }),
     })
   )
 
