ab5229bb9a679ac55d7fcd6fb00a13f870f037a2
Jose Lima <jalimag0206@gmail.com>
2025-02-10 15:47:47 -0300

    fix(frontend): refactor redirect logic to clear toast message on navigation

--- frontend/src/components/side-bar.vue
+++ frontend/src/components/side-bar.vue
@@ -6,11 +6,20 @@
         height="150"
         src="@/assets/grupo-a-logo.png"
       />
-    <v-list-item prepend-icon="mdi-account" @click="$router.push('/')">Alunos</v-list-item>
+    <v-list-item prepend-icon="mdi-account" @click="handleRedirect">Alunos</v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script lang="ts" setup>
-  
+  import { useRouter } from 'vue-router';
+  import { useToastStore } from '@/store/toastStore';
+
+  const router = useRouter();
+  const toastStore = useToastStore();
+
+  const handleRedirect = () => {
+    router.push('/');
+    toastStore.clearToastMessage()
+  };
 </script>
